bf391d783b1ceabcc62800d0134a0d21487669ab
Lindsey Maratta <lindseymaratta@LindseyMaratta-MBP.local>
2017-07-14 08:52:04 -0400

    add images and update layout

---
+++ js.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.button').on('click', function(){
+		$('div').toggleClass('fill-red');
+	});
+})
+
+var speakers = [
+{
+	name: "daniel b",
+	title: "design partner",
+	company: "google ventures"
+},
+
+{
+	name: "bob",
+	title: "designer",
+	company: "place"
+},
+{
+	name: "tom",
+	title: "design manager",
+	company: "buzzfeed"
+}
+
+]
